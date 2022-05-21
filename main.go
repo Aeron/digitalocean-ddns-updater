acf4b780f7bde40163b407fa0066291825526586
Eugene “Aeron” Glybin <aeron@aeron.cc>
2022-05-22 01:29:10 +0700

    Main functionality update
    Heavily refactored. TLS server removed. Rate limiter added. More options and env vars added.

--- main.go
+++ main.go
@@ -4,87 +4,165 @@ import (
 	"context"
 	"crypto/sha512"
 	"encoding/hex"
+	"errors"
 	"flag"
+	"fmt"
 	"log"
+	"math"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/digitalocean/godo"
 	"golang.org/x/oauth2"
+	"golang.org/x/time/rate"
 )
 
-const tokenEnvVar = "DIGITALOCEAN_API_TOKEN"
-const endpoint = "/ddns"
+const (
+	defaultAddress      = ":8080"
+	defaultEndpoint     = "/ddns"
+	defaultLimitRPS     = .01
+	defaultLimitBurst   = 1
+	envVarDOAPIToken    = "DIGITALOCEAN_API_TOKEN"
+	envVarSecurityToken = "SECURITY_TOKEN"
+	envVarLimitRPS      = "LIMIT_RPS"
+	envVarLimitBurst    = "LIMIT_BURST"
+)
 
+var (
+	address    = flag.String("address", defaultAddress, "address to listen on")
+	endpoint   = flag.String("endpoint", defaultEndpoint, "endpoint to handle updates")
+	doAPIToken = flag.String(
+		"digitalocean-api-token",
+		getEnvString(envVarDOAPIToken, ""),
+		"DigitalOcean API token",
+	)
+	securityToken = flag.String(
+		"security-token",
+		getEnvString(envVarSecurityToken, ""),
+		"security token",
+	)
+	limitRPS = flag.Float64(
+		"limit-rps",
+		getEnvFloat64(envVarLimitRPS, defaultLimitRPS),
+		"limit requests per second",
+	)
+	limitBurst = flag.Int(
+		"limit-burst",
+		getEnvInt(envVarLimitBurst, defaultLimitBurst),
+		"limit a single burst size",
+	)
+)
+
+var limiter = rate.NewLimiter(rate.Limit(*limitRPS), *limitBurst)
 var digitalOceanClient *godo.Client
-var tokenHashHex string
 
-func checkRecord(domain string, name string) *godo.DomainRecord {
-	options := &godo.ListOptions{
-		Page:    0,
-		PerPage: 0,
+func getEnvString(key string, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
 	}
-	records, response, err := digitalOceanClient.Domains.Records(context.TODO(), domain, options)
+	return fallback
+}
+
+func getEnvInt(key string, fallback int) int {
+	value, err := strconv.Atoi(getEnvString(key, ""))
+	if err != nil {
+		return fallback
+	}
+	return value
+}
+
+func getEnvFloat64(key string, fallback float64) float64 {
+	value, err := strconv.ParseFloat(getEnvString(key, ""), 64)
+	if err != nil {
+		return fallback
+	}
+	return value
+}
+
+func limit(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !limiter.Allow() {
+			delay := limiter.Reserve().Delay().Seconds()
+			w.Header().Add("Retry-After", fmt.Sprintf("%.0f", math.Ceil(delay)))
+			http.Error(
+				w,
+				http.StatusText(http.StatusTooManyRequests),
+				http.StatusTooManyRequests,
+			)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func checkRecord(domain string, name string) (*godo.DomainRecord, error) {
+	records, response, err := digitalOceanClient.Domains.Records(
+		context.TODO(),
+		domain,
+		&godo.ListOptions{Page: 0, PerPage: 0},
+	)
 
 	if err != nil {
 		log.Print(err)
-		return &godo.DomainRecord{}
+		return &godo.DomainRecord{}, err
 	}
 
 	if response.StatusCode >= http.StatusMultipleChoices {
-		log.Printf("Check failed: %s", response.Status)
-		return &godo.DomainRecord{}
+		log.Println("Check failed:", response.Status)
+		return &godo.DomainRecord{}, errors.New(response.Status)
 	}
 
 	for _, r := range records {
 		if r.Name == name {
 			log.Printf("Record %s.%s found", name, domain)
-			return &r
+			return &r, nil
 		}
 	}
 
 	log.Printf("Record %s.%s not found", name, domain)
-	return &godo.DomainRecord{}
+	return nil, errors.New("Record not found")
 }
 
-func updateDNS(domain string, id int, ip string) bool {
-	updateRequest := &godo.DomainRecordEditRequest{
-		Data: ip,
-	}
-
-	_, response, err := digitalOceanClient.Domains.EditRecord(context.TODO(), domain, id, updateRequest)
+func updateDNS(domain string, id int, ip string) error {
+	_, response, err := digitalOceanClient.Domains.EditRecord(
+		context.TODO(),
+		domain,
+		id,
+		&godo.DomainRecordEditRequest{Data: ip},
+	)
 
 	if err != nil {
-		log.Print(err)
-		return false
+		log.Println("Request failed:", err)
+		return err
 	}
 
 	if response.StatusCode >= http.StatusMultipleChoices {
-		log.Printf("Update failed: %s", response.Status)
-		return false
+		log.Println("Update failed:", response.Status)
+		return errors.New(response.Status)
 	}
 
 	log.Print("Updated successfully")
-	return true
+	return nil
 }
 
-func ddns(res http.ResponseWriter, req *http.Request) {
-	domain := req.URL.Query().Get("domain")
-	token := req.URL.Query().Get("token")
-
-	// ip := strings.Split(req.RemoteAddr, ":")[0]
-	ip := req.URL.Query().Get("ip")
+func ddnsHandler(res http.ResponseWriter, req *http.Request) {
+	query := req.URL.Query()
+	domain := query.Get("domain")
+	token := query.Get("token")
+	ip := query.Get("ip")
 
 	res.Header().Set("Content-Type", "text/plain")
 
 	if domain == "" || token == "" || ip == "" {
-		http.Error(res, "Invalid domain, token or IP.", http.StatusBadRequest)
+		http.Error(res, "Invalid domain, token or IP", http.StatusBadRequest)
 		return
 	}
 
-	if token != tokenHashHex {
-		http.Error(res, "Authentication failed.", http.StatusForbidden)
+	if token != *securityToken {
+		http.Error(res, "Authentication failed", http.StatusUnauthorized)
 		return
 	}
 
@@ -95,60 +173,53 @@ func ddns(res http.ResponseWriter, req *http.Request) {
 	name := parts[0]
 	domain = parts[1]
 
-	record := checkRecord(domain, name)
+	var record *godo.DomainRecord
 
-	if record.ID < 0 {
-		http.Error(res, "No record found.", http.StatusNotFound)
+	if record, err := checkRecord(domain, name); err != nil || record.ID < 0 {
+		http.Error(res, "No record found", http.StatusNotFound)
 		return
 	}
 
-	if !updateDNS(domain, record.ID, ip) {
-		http.Error(res, "Failed.", http.StatusNoContent)
+	if err := updateDNS(domain, record.ID, ip); err != nil {
+		http.Error(res, "Failed", http.StatusNoContent)
 		return
 	}
 
-	res.Write([]byte("Done."))
+	res.Write([]byte("Done"))
 }
 
 func main() {
-	var tlsFlag = flag.Bool("tls", false, "use TLS server")
-	var tlsCert = flag.String("cert", "cert.pem", "path to certificate")
-	var tlsKey = flag.String("key", "key.pem", "path to private key")
-	var token = flag.String("token", os.Getenv(tokenEnvVar), "DigitalOcean API token")
-
 	flag.Parse()
 
-	if *token == "" {
-		log.Fatalf("Invalid token (%s)", tokenEnvVar)
+	if *doAPIToken == "" {
+		log.Fatalln("DigitalOcean API token is required")
 	}
 
-	tokenHash := sha512.Sum512_256([]byte(*token))
-	tokenHashHex = hex.EncodeToString(tokenHash[:])
-
-	log.Printf("Auth token: %s", tokenHashHex)
-
-	tokenPrepared := &oauth2.Token{
-		AccessToken: *token,
+	if *securityToken == "" {
+		hash := sha512.Sum512_256([]byte(*doAPIToken))
+		hashHex := hex.EncodeToString(hash[:])
+		securityToken = &hashHex
 	}
 
-	tokenSource := oauth2.StaticTokenSource(tokenPrepared)
+	tokenPrepared := &oauth2.Token{AccessToken: *doAPIToken}
+	digitalOceanClient = godo.NewClient(
+		oauth2.NewClient(
+			context.Background(),
+			oauth2.StaticTokenSource(tokenPrepared),
+		),
+	)
 
-	oauthClient := oauth2.NewClient(context.Background(), tokenSource)
-	digitalOceanClient = godo.NewClient(oauthClient)
+	mux := http.NewServeMux()
+	mux.HandleFunc(defaultEndpoint, ddnsHandler)
 
-	http.HandleFunc(endpoint, ddns)
+	go func() {
+		log.Println("Starting server on", *address)
+		log.Println("Auth token:", *securityToken)
 
-	var err error
+		if err := http.ListenAndServe(*address, limit(mux)); err != nil {
+			log.Fatalln("Server error:", err)
+		}
+	}()
 
-	if *tlsFlag {
-		log.Print("Running secure server on port 443")
-		err = http.ListenAndServeTLS(":443", *tlsCert, *tlsKey, nil)
-	} else {
-		log.Print("Running insecure server on port 80")
-		err = http.ListenAndServe(":80", nil)
-	}
-
-	if err != nil {
-		log.Fatal(err)
-	}
+	select {}
 }
